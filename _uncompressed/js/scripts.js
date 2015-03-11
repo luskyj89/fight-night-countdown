1ffcdda7ba096f06ae4208b628b1d165f7aa25b0
John Lusky <info@johnlusky.com>
2015-03-11 00:05:35 -0400

    Changed bg video and logo

--- _uncompressed/js/scripts.js
+++ _uncompressed/js/scripts.js
@@ -3,10 +3,10 @@
 // videoBG
 function videoStarter() {
     $('.space').videoBG({
-        mp4:'video/lightning.mp4',
-        ogv:'video/shutterstock_v2529842.ogv',
-        webm:'video/lightning.webm',
-        poster:'video/shutterstock_v2529842.jpg',
+        mp4:'video/smash-taunt-loop.mp4',
+        //ogv:'video/shutterstock_v2529842.ogv',
+        webm:'video/smash-taunt-loop.webm',
+        //poster:'video/shutterstock_v2529842.jpg',
         scale:true,
         zIndex:0
     });
@@ -14,9 +14,8 @@ function videoStarter() {
 
 function init() {
 
-    $('.countdown').countdown('01/31/2015 21:00:00', function(event) {
+    $('.countdown').countdown('03/14/2015 21:00:00', function(event) {
           var $this = $(this).html(event.strftime(''
-            + '<span>%d</span> days '
             + '<span>%H</span> hr '
             + '<span>%M</span> min '
             + '<span>%S</span> sec'));
