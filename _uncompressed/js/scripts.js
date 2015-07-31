9e38b0a8e7f7e40e911cf631998574e29f594f22
John Lusky <lusky@1trickpony.com>
2015-07-31 16:23:50 -0400

    Built new version for FN5

--- _uncompressed/js/scripts.js
+++ _uncompressed/js/scripts.js
@@ -3,9 +3,9 @@
 // videoBG
 function videoStarter() {
     $('.space').videoBG({
-        mp4:'video/lightning.mp4',
+        mp4:'video/388751.mp4',
         ogv:'video/shutterstock_v2529842.ogv',
-        webm:'video/lightning.webm',
+        webm:'video/388751.webm',
         poster:'video/shutterstock_v2529842.jpg',
         scale:true,
         zIndex:0
@@ -14,9 +14,8 @@ function videoStarter() {
 
 function init() {
 
-    $('.countdown').countdown('01/31/2015 21:00:00', function(event) {
+    $('.countdown').countdown('08/01/2015 21:00:00', function(event) {
           var $this = $(this).html(event.strftime(''
-            + '<span>%d</span> days '
             + '<span>%H</span> hr '
             + '<span>%M</span> min '
             + '<span>%S</span> sec'));
